b07ce77d1e85e72e10d7c638350fa38e2f47da22
Flub78 <frederic.peignot@free.fr>
2021-06-09 08:39:08 +0200

    php artisan backup:list is now tenant compatible

--- app/Console/Commands/BackupList.php
+++ app/Console/Commands/BackupList.php
@@ -3,6 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use App\Models\Tenant;
 
 class BackupList extends Command
 {
@@ -11,7 +12,7 @@ class BackupList extends Command
      *
      * @var string
      */
-    protected $signature = 'backup:list';
+    protected $signature = 'backup:list {--all : All tenants} {--tenant= : one tenant}';
 
     /**
      * The console command description.
@@ -31,25 +32,59 @@ class BackupList extends Command
     }
 
     /**
+     * List the backups from a directory
+     * @param string $storage
+     * @param string $context
+     */
+    public function listBackups(string $storage, string $context = "") {
+    	if (!is_dir($storage)) {
+    		echo "directory $storage not found\n";
+    		return;
+    	}
+    	echo "Local backups:$context\n";
+    	$backup_list = scandir($storage);
+    	for ($i = 2; $i < count($backup_list); $i++) {
+    		echo ($i - 1) . ") " . $backup_list[$i] . "\n";
+    	}
+    	echo ".\n";
+    }
+    
+    /**
+     * List the backups for a tenant
+     * @param string $tenant
+     */
+    public function listTenantBackup(string $tenant_id) {
+    	$tnt = Tenant::whereId ( $tenant_id )->first();
+    	
+    	if (!$tnt) {
+    		echo "tenant $tenant_id not found";
+    		return;
+    	}
+    	$database = $tnt['tenancy_db_name'];
+    	$this->listBackups(storage_path() . "/$database/app/backup", "tenant=" . $tenant_id);
+    }
+    
+    /**
      * Execute the console command.
      *
      * @return int
      */
     public function handle()
-    {
-    	$dirpath = storage_path() . "/app/backup";
-    	
-    	if (!is_dir($dirpath)) {
-    		echo "creating " . $dirpath . "\n";
-    		mkdir($dirpath);
+    {  	    	
+    	if ($this->option('tenant')) {
+    		$this->listTenantBackup($this->option('tenant'));
+    		return 0;
     	}
     	
-    	echo "Local backups:\n";
-    	$backup_list = scandir($dirpath);
-    	for ($i = 2; $i < count($backup_list); $i++) {
-    		echo ($i - 1) . ") " . $backup_list[$i] . "\n";
+    	$this->listBackups(storage_path() . "/app/backup", "central database");
+    	
+    	if ($this->option('all')) {
+    		$tenants = Tenant::all();
+    		foreach ($tenants as $tenant) {
+    			$this->listTenantBackup($tenant->id);
+    		}
     	}
-    	echo ".";
+    	   	    	    	
         return 0;
     }
 }
