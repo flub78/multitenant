c760d43ccffa64e2d904d99e354278bc2f0b9efa
Flub78 <frederic.peignot@free.fr>
2021-06-27 22:20:59 +0200

    Work on localization

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -37,7 +37,18 @@
 		    "oLanguage": olanguage
 	    });
 		
-	$( ".datepicker" ).datepicker($.datepicker.regional[ "fr" ]);
+$.datepicker.regional['it'] = {
+    closeText: 'Chiudi', // set a close button text
+    currentText: 'Oggi', // set today text
+    monthNames: ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno',   'Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'], // set month names
+    monthNamesShort: ['Gen','Feb','Mar','Apr','Mag','Giu','Lug','Ago','Set','Ott','Nov','Dic'], // set short month names
+    dayNames: ['Domenica','Luned&#236','Marted&#236','Mercoled&#236','Gioved&#236','Venerd&#236','Sabato'], // set days names
+    dayNamesShort: ['Dom','Lun','Mar','Mer','Gio','Ven','Sab'], // set short day names
+    dayNamesMin: ['Do','Lu','Ma','Me','Gio','Ve','Sa'], // set more short days names
+    dateFormat: 'dd/mm/yy' // set format date
+};
+		
+	$( ".datepicker" ).datepicker($.datepicker.regional['it']);
 	 
 	$('.timepicker').timepicker({
     	timeFormat: 'HH:mm',
