6376d57d799f4983d2af26857161383f7f207474
Flub78 <frederic.peignot@free.fr>
2021-07-13 08:33:16 +0200

    Use environment variable to set database during test

--- config/database.php
+++ config/database.php
@@ -68,7 +68,7 @@ return [
     				'url' => env('DATABASE_URL'),
     				'host' => env('DB_HOST', '127.0.0.1'),
     				'port' => env('DB_PORT', '3306'),
-    				'database' => 'multi_test',
+    				'database' => env('DB_DATABASE', 'multi_test'),
     				'username' => env('DB_USERNAME', 'test_user_default'),
     				'password' => env('DB_PASSWORD', ''),
     				'unix_socket' => env('DB_SOCKET', ''),
