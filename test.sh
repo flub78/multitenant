66e4e03ad46f8533d3d416af7ffb2a1f6a2432ee
Flub78 <frederic.peignot@free.fr>
2021-08-04 16:22:17 +0200

    Enable phpunit coverage per default

--- test.sh
+++ test.sh
@@ -1,3 +1,5 @@
 # No Dusk test run with this script
 # rm tests/Browser/screenshots/*.png
-/usr/bin/php8.0 vendor/bin/phpunit --testdox-html doc/testdox.html --log-junit results/results.xml
+# /usr/bin/php8.0 vendor/bin/phpunit --testdox-html doc/testdox.html --log-junit results/results.xml
+export XDEBUG_MODE=coverage
+/usr/bin/php8.0 vendor/bin/phpunit -d xdebug.mode=coverage -d zend_extension=/usr/lib/php/20200930/xdebug.so --testdox-html doc/testdox.html --coverage-html results/coverage
