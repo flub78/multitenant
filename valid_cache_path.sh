2b67aea557778ab181b9be3db24aa6d7b6c26422
Flub78 <frederic.peignot@free.fr>
2021-12-02 18:58:09 +0100

    Script to make a valid cache path

---
+++ valid_cache_path.sh
@@ -0,0 +1,4 @@
+mkdir storage/framework
+chmod 0777 storage/framework
+mkdir storage/framework/sessions storage/framework/views storage/framework/cache
+chmod 0777 storage/framework/sessions storage/framework/views storage/framework/cache
