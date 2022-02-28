0fb1a830424986797e1c91d5b8642a4c7b88039f
Flub78 <frederic.peignot@free.fr>
2022-02-28 13:08:11 +0100

    DevOps - Test the help for Dev:Stats

--- tests/Feature/DevStatsArtisanTest.php
+++ tests/Feature/DevStatsArtisanTest.php
@@ -19,4 +19,13 @@ class DevStatsArtisanTest extends TestCase {
 		$exitCode = Artisan::call("dev:stats");
 		$this->assertEquals($exitCode, 0, "Error on artisan dev:stats");		
 	}	
+	
+	/**
+	 *
+	 */
+	public function test_dev_stats_help() {
+		$exitCode = Artisan::call("dev:stats --help");
+		$this->assertEquals($exitCode, 0, "Error on artisan dev:stats");
+	}
+	
 }
