9a66df11fe68b0aaeabfbfc9c4494c5a236888e1
Flub78 <frederic.peignot@free.fr>
2022-05-12 07:18:28 +0200

    Improve the dusk test for profile (not automated yet)

--- tests/Browser/Tenants/ProfileTest.php
+++ tests/Browser/Tenants/ProfileTest.php
@@ -5,6 +5,7 @@ namespace Tests\Browser\Tenants;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 use App\Helpers\BackupHelper;
+use App\Models\Tenants\Profile;
 
 /**
  * Profile CRUD
@@ -63,25 +64,81 @@ class ProfileTest extends DuskTestCase {
 			// Press the button to add an element
 			$browser->press ( __('profile.add')  )
 			->assertPathIs('/profile/create');
-			
-            // The rest is not correctly implemented yet
-            return;
-            			
+						
+            // Create an element
+            // $profile = Profile::factory()->make();
+                        
 			// Fill the form to create an element
 			$browser
-			->select ( 'key', 'app.timezone')
-			->type ( 'value', 'Europe/Paris')
+			->type ('first_name', 'John')		
+			->type ('last_name', 'Doe')
+			->type ('birthday', '08-29-1959' . "\n")
+			->select('user_id');
+			
+			// Validate and check the result
+			$browser
 			->press ( 'Submit' )
 			->assertPathIs('/profile')
-			->assertSee ( 'Paris' )
 			->assertSee ( "Showing 1 to 1 of 1 entries" );
 			
+			$browser->assertSee('John');
+			
+			// Create another element
+			
+			// Use it to update the initial element
+			// Goto the index page
+			$browser->visit ( '/profile/1/edit' )
+			->assertSee ( __('general.edit') )
+			->assertSee ( __('profile.elt') );
+			
+			$browser
+			->type ('first_name', 'Joe')
+			->type ('last_name', 'Dalton')
+			->type ('birthday', '08-29-1959' . "\n")
+			->select('user_id');
+			$browser->screenshot('Tenants/profile_edit_form');
+			
+						
+			// Check that the first element has been modified
+			// Validate and check the result
+			$browser
+			->press ( 'Update' )
+			->assertPathIs('/profile')
+			->assertSee('Dalton')
+			->assertSee ( "Showing 1 to 1 of 1 entries" );
+			$browser->screenshot('Tenants/profile_after_update');
 			
 			// delete
-			$browser->press('Delete')
+			$browser
+			->visit ( '/profile' );
+			$browser->screenshot('Tenants/profile_deleting');
+			
+			$browser
+			->press('Delete')
 			->assertPathIs('/profile')
-			->assertSee ( 'app.timezone deleted' )
+			->assertSee ( 'deleted' )
 			->assertSee ( 'Showing 0 to 0 of 0 entries' );
+		} );
+	}
+	
+	/**
+	 * Check that incorrect values are rejected
+	 *
+	 * @return void
+	 */
+	public function test_validation() {
+		$this->browse ( function (Browser $browser) {
+			
+			$browser->visit ( '/profile/create' )
+			->assertSee ( __('profile.new'));
+
+			// Validate without filling required fields
+			
+			$browser
+			->press ( 'Submit' )
+			->assertPathIs('/profile/create')
+			->assertSee ( "is required" );
+			
 		} );
 	}
 	
