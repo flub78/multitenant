a79154832d8613db8218a615100c913af6fab485
flub78 <frederic.peignot>
2023-04-24 07:37:13 +0200

    a script to fix the invalid cache path on windows

---
+++ valid_cache_path.bat
@@ -0,0 +1,7 @@
+mkdir storage\framework
+mkdir storage\framework\sessions
+mkdir storage\framework\views
+mkdir storage\framework\cache
+chmod 0777 storage\framework
+mkdir "storage\framework\sessions storage\framework\views storage\framework\cache"
+chmod 0777 "storage\framework\sessions storage\framework\views storage\framework\cache"
