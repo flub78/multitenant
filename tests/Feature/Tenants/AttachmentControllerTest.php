577b079118e173430853aab95a345ea2cf0958f9
Flub78 <frederic.peignot@free.fr>
2024-12-23 14:32:48 +0100

    Fix some tests

--- tests/Feature/Tenants/AttachmentControllerTest.php
+++ tests/Feature/Tenants/AttachmentControllerTest.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is generated from a template with metadata extracted from the data model.
  * If modifications are required, it is important to consider if they should be done in the template
@@ -50,45 +51,45 @@ use App\Helpers\CodeGenerator as CG;
 class AttachmentControllerTest extends TenantTestCase {
 
     protected $tenancy = true;
-    
-	protected $basename = "attachment";	
-	
-	function __construct() {
-		parent::__construct();
-		
-		// required to be able to use the factory inside the constructor
-		$this->createApplication();
-				
-		$this->user = User::factory()->make();
-		$this->user->admin = true;
-	}
-	
-	function __destruct() {
-		$this->user->delete();
-	}		
 
-	
-	/**
-	 * 
-	 * @param string $segments
-	 * @return string
-	 */
-	protected function base_url($segments = "") {
-		$url = "/" . $this->basename;
-		if ($segments) {
-			$url = join("/", [$url, $segments]);
-		}
-		return $url;
-	}
-	
-	/**
-	 * Return the number of elements in the table managed by the CRUD controller under test
-	 * @return int
-	 */
-	protected function eltCount() {
-		return Attachment::count(); 
-	}
-    
+    protected $basename = "attachment";
+
+    function __construct(?string $name = null) {
+        parent::__construct($name);
+
+        // required to be able to use the factory inside the constructor
+        $this->createApplication();
+
+        $this->user = User::factory()->make();
+        $this->user->admin = true;
+    }
+
+    function __destruct() {
+        $this->user->delete();
+    }
+
+
+    /**
+     * 
+     * @param string $segments
+     * @return string
+     */
+    protected function base_url($segments = "") {
+        $url = "/" . $this->basename;
+        if ($segments) {
+            $url = join("/", [$url, $segments]);
+        }
+        return $url;
+    }
+
+    /**
+     * Return the number of elements in the table managed by the CRUD controller under test
+     * @return int
+     */
+    protected function eltCount() {
+        return Attachment::count();
+    }
+
     /**
      * Test display of all elements
      * Given the user is logged on
@@ -97,16 +98,16 @@ class AttachmentControllerTest extends TenantTestCase {
      * 
      * @return void
      */
-    public function testBaseUrlDisplaysTableView() {   
+    public function testBaseUrlDisplaysTableView() {
         Log::Debug(__METHOD__);
-        
+
         $look_for = [__('attachment.title'), tenant('id')];
-        $look_for[] = __('attachment.referenced_table'); 
-        $look_for[] = __('attachment.referenced_id'); 
-        $look_for[] = __('attachment.user_id'); 
-        $look_for[] = __('attachment.filename'); 
-        $look_for[] = __('attachment.description'); 
-        $look_for[] = __('attachment.file'); 
+        $look_for[] = __('attachment.referenced_table');
+        $look_for[] = __('attachment.referenced_id');
+        $look_for[] = __('attachment.user_id');
+        $look_for[] = __('attachment.filename');
+        $look_for[] = __('attachment.description');
+        $look_for[] = __('attachment.file');
 
         $this->get_tenant_url($this->user, 'attachment', $look_for);
     }
@@ -119,9 +120,9 @@ class AttachmentControllerTest extends TenantTestCase {
      */
     public function testCreateUrlDisplaysCreationForm() {
         Log::Debug(__METHOD__);
-        $this->get_tenant_url($this->user, 'attachment/create', [__('attachment.new')]);   
-     }
-    
+        $this->get_tenant_url($this->user, 'attachment/create', [__('attachment.new')]);
+    }
+
     /**
      * Test display of one element
      * Given the user is logged on
@@ -131,17 +132,17 @@ class AttachmentControllerTest extends TenantTestCase {
      * 
      * @return void
      */
-    public function testGetRequestShowsElement() {        
+    public function testGetRequestShowsElement() {
         Log::Debug(__METHOD__);
-        
+
         Attachment::factory()->create();
         $latest = Attachment::latest()->first();
-        
+
         $id = $latest->id;
-        
+
         $this->get_tenant_url($this->user, 'attachment/' . $id);
     }
-    
+
     /**
      * Test a post request
      * Given the user is logged on
@@ -150,30 +151,29 @@ class AttachmentControllerTest extends TenantTestCase {
      * 
      * @return void
      */
-    public function testPostRequestStoresElement()
-    {    	        
+    public function testPostRequestStoresElement() {
         Log::Debug(__METHOD__);
-        
+
         // Post a creation request
         $attachment = Attachment::factory()->make();
         $elt = ['_token' => csrf_token()];
-        $elt['referenced_table'] = $attachment->referenced_table; 
-        $elt['referenced_id'] = $attachment->referenced_id; 
-        $elt['user_id'] = $attachment->user_id; 
-        $elt['filename'] = $attachment->filename; 
-        $elt['description'] = $attachment->description; 
-        $elt['file'] = $attachment->file; 
+        $elt['referenced_table'] = $attachment->referenced_table;
+        $elt['referenced_id'] = $attachment->referenced_id;
+        $elt['user_id'] = $attachment->user_id;
+        $elt['filename'] = $attachment->filename;
+        $elt['description'] = $attachment->description;
+        $elt['file'] = $attachment->file;
 
-        $initial_count = Attachment::count ();
-        
+        $initial_count = Attachment::count();
+
         // call the post method to create it
         $this->post_tenant_url($this->user, 'attachment', ['created'], $elt);
-        
-        $new_count = Attachment::count ();
+
+        $new_count = Attachment::count();
         $expected = $initial_count + 1;
-        $this->assertEquals ( $expected, $new_count, "attachment created, actual=$new_count, expected=$expected" );       
+        $this->assertEquals($expected, $new_count, "attachment created, actual=$new_count, expected=$expected");
     }
-    
+
     /**
      * Test an invalid post request
      * 
@@ -188,23 +188,23 @@ class AttachmentControllerTest extends TenantTestCase {
 
         $cnt = 1;
         foreach (Attachment::factory()->error_cases() as $case) {
-            $initial_count = Attachment::count ();
-                
+            $initial_count = Attachment::count();
+
             $elt = ['_token' => csrf_token()];
             $elt = array_merge($elt, $case["fields"]);
-        
-            $response = $this->post_tenant_url( $this->user, 'attachment', [], $elt, $errors_expected = true);
+
+            $response = $this->post_tenant_url($this->user, 'attachment', [], $elt, $errors_expected = true);
             // $response->dumpSession();
-        
+
             $response->assertSessionHasErrors($case["errors"]);
-        
-            $new_count = Attachment::count ();
-            $this->assertEquals ( $initial_count, $new_count, "error case $cnt: attachment not created, actual=$new_count, expected=$initial_count" );
+
+            $new_count = Attachment::count();
+            $this->assertEquals($initial_count, $new_count, "error case $cnt: attachment not created, actual=$new_count, expected=$initial_count");
             $cnt = $cnt + 1;
         }
         $this->assertTrue($cnt == 1 + count(Attachment::factory()->error_cases()));
     }
-    
+
 
     /**
      * Check that the edit form is correctly displayed
@@ -214,13 +214,13 @@ class AttachmentControllerTest extends TenantTestCase {
      * 
      * @return void
      */
-    public function testEditUrlDisplaysPopulatedEditForm() {        
+    public function testEditUrlDisplaysPopulatedEditForm() {
         Log::Debug(__METHOD__);
-        
+
         Attachment::factory()->create();
         $latest = Attachment::latest()->first();
         $id = $latest->id;
-        
+
         $this->get_tenant_url($this->user, 'attachment/' . $id . '/edit', [__('general.edit') . " " . __('attachment.elt')]);
     }
 
@@ -233,46 +233,46 @@ class AttachmentControllerTest extends TenantTestCase {
      * 
      * @return void
      */
-    public function testPostRequestUpdatesElement() {   
+    public function testPostRequestUpdatesElement() {
         Log::Debug(__METHOD__);
-        
+
         $attachment = Attachment::factory()->make();                // create an element
         $attachment2 = Attachment::factory()->make();               // and a second one
         $elt = ['_token' => csrf_token()];
         $elt2 = ['_token' => csrf_token()];
-        
-        foreach ([ "referenced_table", "referenced_id", "user_id", "filename", "description", "file" ] as $field) {
+
+        foreach (["referenced_table", "referenced_id", "user_id", "filename", "description", "file"] as $field) {
             $elt[$field] = $attachment->$field;
             $elt2[$field] = $attachment2->$field;
         }
-        
+
         $attachment->save();                            // save the first element
         $latest = Attachment::latest()->first();
         $this->assertNotNull($latest);
         $id = $latest->id;
         $this->assertNotNull($id);
-        
+
         $initial = Attachment::where('id', $id)->first();     // get it back
         $this->assertNotNull($initial);
-        
+
         $table = "attachments";
         // Check that the first saved element has the correct values and is different from the second one
-        foreach ([ "referenced_table", "referenced_id", "user_id", "filename", "description", "file" ] as $field) {
+        foreach (["referenced_table", "referenced_id", "user_id", "filename", "description", "file"] as $field) {
             if ($field != 'id') {
                 $this->assertEquals($initial->$field, $elt[$field], "correct field $field retreived from the database");
                 if (CG::lot_of_values($table, $field))
                     $this->assertNotEquals($initial->$field, $attachment2->$field, "field $field is different between two random instances");
             }
         }
-                
+
         // Update the values using the second element
         $elt2['id'] = $id;
         $this->patch_tenant_url($this->user, 'attachment/' . $id, $elt2);
-        
+
         $updated = Attachment::where('id', $id)->first();
-        $this->assertNotNull($updated);     
-        foreach ([ "referenced_table", "referenced_id", "user_id", "filename", "description", "file" ] as $field) {
-            if ($field != 'id' && CG::testable($table, $field) ) {
+        $this->assertNotNull($updated);
+        foreach (["referenced_table", "referenced_id", "user_id", "filename", "description", "file"] as $field) {
+            if ($field != 'id' && CG::testable($table, $field)) {
                 $this->assertEquals($updated->$field, $elt2[$field]);
             }
         }
@@ -289,24 +289,24 @@ class AttachmentControllerTest extends TenantTestCase {
      */
     public function testDeleteRequestsRemovesElement() {
         Log::Debug(__METHOD__);
-        
-        $initial_count = Attachment::count ();
+
+        $initial_count = Attachment::count();
 
         Attachment::factory()->create();
         $latest = Attachment::latest()->first();
         $id = $latest->id;
-        
-        $new_count = Attachment::count ();
+
+        $new_count = Attachment::count();
         $expected = $initial_count + 1;
-        $this->assertEquals ( $expected, $new_count, "one attachment created, actual=$new_count, expected=$expected" );
-        
+        $this->assertEquals($expected, $new_count, "one attachment created, actual=$new_count, expected=$expected");
+
         $this->delete_tenant_url($this->user, 'attachment/' . $id, ['deleted']);
-        
-        $count_after_delete = Attachment::count ();
+
+        $count_after_delete = Attachment::count();
         $expected = $initial_count;
-        $this->assertEquals ( $expected, $count_after_delete, "attachment deleted, actual=$count_after_delete, expected=$expected" );             
+        $this->assertEquals($expected, $count_after_delete, "attachment deleted, actual=$count_after_delete, expected=$expected");
     }
-   
+
     /**
      * Test not found URL
      * Given the user is logged on
@@ -333,38 +333,38 @@ class AttachmentControllerTest extends TenantTestCase {
      */
     public function testPagesAreDisplayedAccordingToLocaleLanguage() {
         Log::Debug(__METHOD__);
-        
+
         /**
          * Scenario: Attachment testLocale fr
          * Given the local is set to fr
          * When calling URLs
          * Then views are displayed in French
          */
-        
+
         $this->be($this->user);
-        
+
         $locale = App::getLocale();
 
-    	App::setLocale('fr');
-        $this->get_tenant_url($this->user, 'attachment/create', [__('attachment.new')]);      
+        App::setLocale('fr');
+        $this->get_tenant_url($this->user, 'attachment/create', [__('attachment.new')]);
         $fr_string = __('attachment.new');
 
-    	/**
-    	 * Scenario: Attachment testLocale en
-    	 * Given the local is set to en
-    	 * When calling URLs
-    	 * Then views are displayed in English
-    	 */
-    	
-    	App::setLocale('en');
-        $this->get_tenant_url($this->user, 'attachment/create', [__('attachment.new')]);      
+        /**
+         * Scenario: Attachment testLocale en
+         * Given the local is set to en
+         * When calling URLs
+         * Then views are displayed in English
+         */
+
+        App::setLocale('en');
+        $this->get_tenant_url($this->user, 'attachment/create', [__('attachment.new')]);
         $en_string = __('attachment.new');
-        $this->get_tenant_url($this->user, 'attachment', [__('attachment.title')]);      
+        $this->get_tenant_url($this->user, 'attachment', [__('attachment.title')]);
         $en_string = __('attachment.title');
-       $this->assertNotEquals($fr_string, $en_string);
-   	
-    	App::setLocale($locale);
-    	$new_locale = App::getLocale();
-    	$this->assertTrue($new_locale == $locale, "Locale back to initial value");	
-    }    
+        $this->assertNotEquals($fr_string, $en_string);
+
+        App::setLocale($locale);
+        $new_locale = App::getLocale();
+        $this->assertTrue($new_locale == $locale, "Locale back to initial value");
+    }
 }
